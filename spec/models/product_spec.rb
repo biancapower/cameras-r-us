66429008222648da2efbdf365ad991044245fc10
biancapower <bianca@biancapower.com>
2022-05-28 21:02:40 +1000

    implement model validation tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a product name, brand name, and retail price" do
+    product = Product.new(
+      product_name: "Camcorder 360",
+      brand_name: "Koduck",
+      retail_price: 52.99
+    )
+    expect(product).to be_valid
+  end
+
+  it "is invalid without a product name" do
+    product = Product.new(product_name: nil)
+    product.valid?
+    expect(product.errors[:product_name]).to include("can't be blank")
+  end
+
+  it "is invalid without a brand name" do
+    product = Product.new(brand_name: nil)
+    product.valid?
+    expect(product.errors[:brand_name]).to include("can't be blank")
+  end
+
+  it "is invalid if retail price is not numerical" do
+    product = Product.new(retail_price: "fred")
+    product.valid?
+    expect(product.errors[:retail_price]).to include("is not a number")
+  end
 end
--- spec/models/retailer_spec.rb
+++ spec/models/retailer_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Retailer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a name and address" do
+    retailer = Retailer.new(
+      name: "Camcorder 360",
+      address: "123 Main Street, Beeac, VIC 3251"
+    )
+    expect(retailer).to be_valid
+  end
+
+  it "is invalid without a name" do
+    retailer = Retailer.new(name: nil)
+    retailer.valid?
+    expect(retailer.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without an address" do
+    retailer = Retailer.new(address: nil)
+    retailer.valid?
+    expect(retailer.errors[:address]).to include("can't be blank")
+  end
 end
