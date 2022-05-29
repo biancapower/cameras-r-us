23c034b77a876eb97bae49070a1e02286dfbf5a4
biancapower <bianca@biancapower.com>
2022-05-29 19:21:24 +1000

    update retailer model tests to match changes to validation and custom messages

--- spec/models/retailer_spec.rb
+++ spec/models/retailer_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Retailer, type: :model do
-  it "is valid with a name and address" do
+  it "is valid with a name, address and maps_url" do
     retailer = Retailer.new(
       name: "Camcorder 360",
-      address: "123 Main Street, Beeac, VIC 3251"
+      address: "123 Main Street, Beeac, VIC 3251",
+      maps_url: "https://maps.google.com/?q=123+Main+St,+Beeac+VIC+3251,+Australia&ftid=0x6ad3a6b81f62c6d3:0x29be25d25de5c459"
     )
     expect(retailer).to be_valid
   end
@@ -12,12 +13,15 @@ RSpec.describe Retailer, type: :model do
   it "is invalid without a name" do
     retailer = Retailer.new(name: nil)
     retailer.valid?
-    expect(retailer.errors[:name]).to include("can't be blank")
+    expect(retailer.errors.values).to include("Please enter a name")
   end
 
-  it "is invalid without an address" do
-    retailer = Retailer.new(address: nil)
+  it "is invalid without a maps_url" do
+    retailer = Retailer.new(
+      name: "Camcorder 360",
+      address: "123 Main Street, Beeac, VIC 3251"
+    )
     retailer.valid?
-    expect(retailer.errors[:address]).to include("can't be blank")
+    expect(retailer.errors.values).to include("Please select a valid address from the dropdown")
   end
 end
